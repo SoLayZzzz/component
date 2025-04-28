5854428727530f8cc10a634da314dea05d5cb3b2
Lay <Kremsoklay@gmail.com>
2025-04-28 11:27:21 +0700

    Update component

---
+++ lib/newText.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class NewTextComponent extends StatelessWidget {
+  const NewTextComponent({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Text("New Text Component");
+  }
+}
